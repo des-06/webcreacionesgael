23948b08e7599f08c3c338bca8a60575338cd8c7
des-06 <desbg@outlook.es>
2023-03-27 19:52:39 -0500

    vendor added

--- dbconnection.php
+++ dbconnection.php
@@ -1,7 +1,7 @@
 <?php
-    // require __DIR__ . '/vendor/autoload.php';
-    // $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    // $dotenv->load();
+    require __DIR__ . '/vendor/autoload.php';
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
     
     $mysqli = mysqli_init();
     $mysqli -> real_connect($_ENV['DB_HOST'],$_ENV['DB_USER'],$_ENV['DB_PASSWORD'],$_ENV['DB_DATABASE'], $_ENV['DB_PORT']);
